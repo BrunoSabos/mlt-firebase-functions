92cec403a76f990ee842e81e33329c0a1faab68e
Mickael MAGNIEZ <mmagniez@vente-privee.com>
2019-04-10 16:33:37 +0200

    wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,19 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.region('europe-west1').https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.region('europe-west1').https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+export const translate = functions.region('europe-west1').https.onRequest((req, res) => {
+    // const original = req.query.text;
+    res.json(
+        {data: {response: "bonjour"}}
+    )
+})
+export const getWord = functions.region('europe-west1').https.onRequest((req, res) => {
+    // const original = req.query.text;
+    const words = ["hello", "world"]
+    res.json(
+        {data: {word: words[Math.floor(Math.random() * words.length)]}}
+    )
+})
